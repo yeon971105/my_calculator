40674138612f293edd3e2919216ab7a4a60edc12
Jewon Yeon <yeon971105@icloud.com>
2024-10-09 19:59:34 -0700

    F

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="my_calculator_jyy",
-    version="1.0.1",
+    version="0.0.1",
     packages=find_packages(),
     description="A comprehensive calculator module for various mathematical operations",
     long_description=open('README.md').read(),
